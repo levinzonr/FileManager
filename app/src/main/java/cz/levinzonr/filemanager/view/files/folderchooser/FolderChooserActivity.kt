9218010bdb1718b2d955e44b7ead61c25e7a6a3c
Roman Levinzon <levinzon.roman@gmail.com>
2018-04-01 14:40:38 +0200

    Folder selection implemented

--- app/src/main/java/cz/levinzonr/filemanager/view/files/folderchooser/FolderChooserActivity.kt
+++ app/src/main/java/cz/levinzonr/filemanager/view/files/folderchooser/FolderChooserActivity.kt
@@ -1,9 +1,11 @@
 package cz.levinzonr.filemanager.view.files.folderchooser
 
+import android.app.FragmentTransaction
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import cz.levinzonr.filemanager.R
@@ -11,8 +13,11 @@ import cz.levinzonr.filemanager.model.File
 import cz.levinzonr.filemanager.view.files.BaseFileListFragment
 
 import kotlinx.android.synthetic.main.activity_folder_chooser.*
+import kotlinx.android.synthetic.main.content_folder_chooser.*
 
 class FolderChooserActivity : AppCompatActivity(), BaseFileListFragment.OnFilesFragmentInteraction {
+
+    private lateinit var path: String
 
     companion object {
 
@@ -20,6 +25,7 @@ class FolderChooserActivity : AppCompatActivity(), BaseFileListFragment.OnFilesF
         const val ARG_PATH = "InitialPath"
         const val RESULT_OK = 1
         const val RESULT_CANCELED = 0
+        const val SAVED_PATH = "SavedPathh"
 
 
         fun startForResult(appCompatActivity: AppCompatActivity, string: String, rc: Int) {
@@ -34,11 +40,22 @@ class FolderChooserActivity : AppCompatActivity(), BaseFileListFragment.OnFilesF
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_folder_chooser)
         setSupportActionBar(toolbar)
-        val path = intent.extras.getString(ARG_PATH)
+
+        path = intent.extras.getString(ARG_PATH)
+        if (savedInstanceState == null)
             supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.container, BaseFileListFragment.newInstance(path))
                     .commit()
+
+        else
+            path = savedInstanceState.getString(SAVED_PATH)
+
+        button_confirm.setOnClickListener({
+            val result = Intent(ACTION, Uri.parse(path))
+            setResult(RESULT_OK, result)
+            finish()
+        })
 
     }
 
@@ -46,9 +63,18 @@ class FolderChooserActivity : AppCompatActivity(), BaseFileListFragment.OnFilesF
         Toast.makeText(this, "not a folder", Toast.LENGTH_SHORT).show()
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putString(SAVED_PATH, path)
+    }
+
     override fun onDirectorySelected(file: File) {
-        val result = Intent(ACTION, Uri.parse(file.path))
-        setResult(RESULT_OK, result)
-        finish()
+        path = file.path
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, BaseFileListFragment.newInstance(path))
+                .addToBackStack(null)
+                .setTransition(FragmentTransaction.TRANSIT_ENTER_MASK)
+                .commit()
     }
 }
